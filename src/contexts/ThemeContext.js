2ad0b9125de6586427653da46f9553431bc969f6
Israa Alnazer <israa_alnazer@hotmail.com>
2022-04-23 11:30:04 +0200

    Add the ThemeContext provider

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from "react";
+import useToggle from "../hooks/useToggle";
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider(props) {
+    const [isDarkMode, toggleTheme] = useToggle(true);
+    return (
+        // the value will be accessed by this context component children
+        <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
