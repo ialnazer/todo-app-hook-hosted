62241cd2a8e24368d96413147daeaceb4431568b
Israa Alnazer <israa_alnazer@hotmail.com>
2022-04-24 11:06:23 +0200

    Styling the scrolling

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -16,16 +16,14 @@ import TodoForm from './TodoForm';
 import { ThemeContext } from "./contexts/ThemeContext";
 
 function ScrollTop(props) {
-    const { children, window } = props;
+    const { children } = props;
     // Note that you normally won't need to set the window ref as useScrollTrigger
     // will default to window.
     // This is only being set here because the demo is in an iframe.
     const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
         disableHysteresis: true,
         threshold: 100,
     });
-
     const handleClick = (event) => {
         const anchor = (event.target.ownerDocument || document).querySelector(
             '#back-to-top-anchor',
@@ -51,41 +49,42 @@ function ScrollTop(props) {
     );
 }
 
-ScrollTop.propTypes = {
-    children: PropTypes.element.isRequired,
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window: PropTypes.func,
-};
-
 function TodoApp(props) {
+    const [scrollY, setScrollY] = React.useState(0)
 
+    React.useLayoutEffect(() => {
+        const handleScroll = e => {
+            setScrollY(window.scrollY)
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
     // const initialTodos = [];
     // const { todos, addTodo, deleteTodo, editTodo, toggleTodo } = useTodoState(initialTodos);
     const { isDarkMode, toggleTheme } = useContext(ThemeContext);
-
     return (
         <Paper
             style={{
                 padding: 0,
                 margin: 0,
-                height: '100vh',
-                backgroundColor: isDarkMode ? 'rgba(0, 0, 0, 0.7)' : '#fafafa',
-                overflow: 'scroll'
+                height: scrollY==0 ? '100vh' : '',
+                backgroundColor: isDarkMode ? 'rgba(0, 0, 0, 0.7)' : '#fafafa'
             }}
             elevation={0}
         >
             <AppBar color={isDarkMode ? 'default' : 'primary'} position='static' style={{ height: '64px' }}>
                 <ToolBar>
                     <Typography variant='h5' color='inherit'>Todo List</Typography>
-                    <div style={{flexGrow: 1}}/>
-                    <Switch onChange={toggleTheme}/>
+                    <div style={{ flexGrow: 1 }} />
+                    <Switch onChange={toggleTheme} />
                 </ToolBar>
             </AppBar>
             <ToolBar id="back-to-top-anchor" />
-            <Grid container justifyContent='center' style={{ marginTop: '1rem' }}>
+            <Grid container justifyContent='center' style={{ marginTop: '1rem', marginBottom: '1rem' }}>
                 <Grid item xs={11} md={8} lg={4}>
                     <TodoForm />
                     <TodoList />
